012b5d45299819f4a1a23b3fe7b79ffb7bfc5750
Tanner Farrelly <tannerfarrelly@gmail.com>
2022-03-21 18:54:26 -0400

    Add body-parser npm into folder

--- express_server.js
+++ express_server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const PORT = 8080;
+const bodyParser = require('body-parser');
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
 const urlDatabase = {
@@ -18,18 +20,25 @@ app.get('/urls.json', (req, res) => {
 });
 
 app.get('/urls', (req, res) => {
-  const templateVars = {urls: urlDatabase};
+  const templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
 });
 
-app.get('/urls/:shortURL', (req,res) => {
-  const templateVars = {shortURL: req.params.shortURL, longURL : urlDatabase[req.params.shortURL]};
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+});
+
+
+// app.get('/hello', (req, res) => {
+//   res.send('<html><body>Hello <b>World</b></body></html>\n');
+// });
+
+app.get('/urls/:shortURL', (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render('urls_show', templateVars);
 });
 
-app.get('/hello', (req, res) => {
-  res.send('<html><body>Hello <b>World</b></body></html>\n');
-});
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`)
