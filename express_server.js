0371ac5f6dbac4120abb04870f54a0c6e93cf3b7
Tanner Farrelly <tannerfarrelly@gmail.com>
2022-03-22 13:54:06 -0400

    Add working delete button

--- express_server.js
+++ express_server.js
@@ -38,6 +38,11 @@ app.get('/urls/new', (req, res) => {
   res.render('urls_new');
 });
 
+app.get('/u/:shortURL', (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL]
+  res.redirect(longURL);
+});
+
 
 app.post('/urls', (req, res) => {
   const shortURL = generateRandomString()
@@ -45,9 +50,9 @@ app.post('/urls', (req, res) => {
   res.redirect(`/urls/${shortURL}`);
 });
 
-app.get('/u/:shortURL', (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL]
-  res.redirect(longURL);
+app.post('/urls/:shortURL/delete', (req,  res) => { console.log('something')
+  delete urlDatabase[req.params.shortURL];
+res.redirect('/urls');
 });
 
 // Will probably get rid of this.
