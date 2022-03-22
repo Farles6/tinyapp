a835ad287b6ab029f895494dc4d479f4cb17c956
Tanner Farrelly <tannerfarrelly@gmail.com>
2022-03-22 19:14:48 -0400

    Add if statement to check if logged in or not, to display the correct screen

--- express_server.js
+++ express_server.js
@@ -24,21 +24,13 @@ function generateRandomString() {
 };
 
 
-app.get('/', (req, res) => {
-  res.send('Hello!');
-});
-
-app.get('/urls.json', (req, res) => {
-  res.json(urlDatabase);
-});
-
 app.get('/urls', (req, res) => {
   const templateVars = { username: req.cookies['username'], urls: urlDatabase };
   res.render('urls_index', templateVars);
 });
 
 app.get('/urls/new', (req, res) => {
-  const templateVars = {username: req.cookies['username']};
+  const templateVars = { username: req.cookies['username'] };
   res.render('urls_new', templateVars);
 });
 
@@ -59,21 +51,21 @@ app.post('/urls/:shortURL/delete', (req, res) => {
 });
 
 app.post('/login', (req, res) => {
+  let user = req.body.username;
+  res.cookie('username', user);
+  res.redirect('/urls');
+});
+
+app.post('/logout', (req, res) => {
   let user = req.body.username
-  res.cookie('username', user)
+  res.clearCookie('username', user);
   res.redirect('/urls');
 });
 
 app.post('/urls/:shortURL', (req, res) => {
- 
   res.redirect(`/urls/${req.params.shortURL}`);
 });
 
-
-// Will probably get rid of this.
-// app.get('/hello', (req, res) => {
-//   res.send('<html><body>Hello <b>World</b></body></html>\n');
-// });
 
 app.get('/urls/:shortURL', (req, res) => {
   const templateVars = { username: req.cookies['username'], shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
